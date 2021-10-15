e59bfbd75debdc81ae7fa3bb3b10e588faadcfc8
appcypher <appcypher@outlook.com>
2021-10-15 17:42:50 +0100

    Add new pages

---
+++ src/pages/createWorkspace.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template lang="pug">
+  form
+    input(type="text", placeholder="")
+    input(type="text", placeholder="")
+</template>
+
+<style lang="scss"></style>
